8a6c04969974b86701f6cf48db75bbf41efed8bd
Faustix19 <82886402+Faustix19@users.noreply.github.com>
2021-10-09 14:25:02 -0400

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Arreglos1
+{
+	public class Programa
+	{
+		public static void Main(string[] args)
+		{
+			float[] valores = { 46,35,20,15,55,77,99 };
+			float media = valores.Average();
+			Console.WriteLine("La media de los valores es: " + media);
+		}
+	}
+}
