ed38be90f395b9bebb7ac2099283af1e7bac2d92
Faustix19 <82886402+Faustix19@users.noreply.github.com>
2021-10-09 14:17:56 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cadenas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string cadena;
+
+            Console.WriteLine("Ingrese una frase cualquiera ");
+            cadena = Console.ReadLine();
+
+            char[] partes = { ' ' };
+            string[] frase = cadena.Split(partes);
+            int i;
+            for (i = 0; i < frase.Length; i++) 
+            {
+
+                Console.WriteLine("palabra {0}={1}  ",(i+0), frase[i]);
+            }
+            Console.ReadKey();
+        }
+		
+    }
+}
